0bf77f3047ae55932c65888a67fd2177ee920b57
liuya <yang.liu@jax.org>
2021-05-28 22:47:11 -0400

    docker 1.4

---
+++ do_build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=build.docker
+#SBATCH -p compute
+#SBATCH -q batch
+#SBATCH -N 1 # number of nodes
+#SBATCH -n 2 # number of cores
+#SBATCH --mem=20G
+#SBATCH --time=72:00:00
+#SBATCH -o %x.%j.out
+#SBATCH -e %x.%j.err
+
+set -x
+
+gcloud builds submit --tag us.gcr.io/jax-nanopore-01/nanome:v1.4 --timeout=4000s
