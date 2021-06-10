d47399bf6aa727bfff164e3dab4e79d6458b9908
liuya <yang.liu@jax.org>
2021-06-10 11:13:02 -0400

    run DeepMod on google cloud

--- run_on_google_cloud_submit.sh
+++ run_on_google_cloud_submit.sh
@@ -4,10 +4,11 @@ set -x
 date;hostname;pwd
 
 
-sbatch --job-name=NA12878.chr1-3_f3.nxf.gcloud run_on_google_cloud.sbatch CHR1_3_f3 inputs/na12878.google_cloud.chr1-3_f3.filelist.txt
+sbatch --job-name=NA12878.chr6.nxf.gcloud run_on_google_cloud.sbatch CHR6 inputs/na12878.google_cloud.chr6.filelist.txt
+
+#sbatch --job-name=NA12878.chr1-3_f3.nxf.gcloud run_on_google_cloud.sbatch CHR1_3_f3 inputs/na12878.google_cloud.chr1-3_f3.filelist.txt
 
 # sbatch --job-name=NA12878.chr1-3.nxf.gcloud run_on_google_cloud.sbatch CHR1_3 inputs/na12878.google_cloud.chr1-3.filelist.txt
-
 
 #sbatch --job-name=NA12878.chr4-6.nxf.gcloud run_on_google_cloud.sbatch CHR4_6 inputs/na12878.google_cloud.chr4-6.filelist.txt
 
