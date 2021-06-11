8eecaa41a9bb04a738e9c0c31196c16fc5804e6b
liuya <yang.liu@jax.org>
2021-06-11 13:47:35 -0400

    Sanity check finished

--- src/nanocompare/sanity_check_deepmod.py
+++ src/nanocompare/sanity_check_deepmod.py
@@ -5,7 +5,6 @@ Sanity check NA12878 for DeepMod.
 """
 import argparse
 from multiprocessing import Pool
-
 from nanocompare.eval_common import *
 
 
@@ -79,35 +78,6 @@ def report_meth_unmeth_table():
 
         dataset['Unmeth.cov10'].append(ret[6])
         dataset['Meth.cov10'].append(ret[7])
-
-    # for chr in humanChrSet:
-    #     if chr in ['chr22', 'chrY']:
-    #         continue
-    #     chrSet = [chr]
-    #     # Combine one/two replicates using DeepMod methods
-    #     bgTruth1, ret1 = combineBGTruthListUsingDeepMod(bgTruthList, covCutoff=1, filterChrs=chrSet)
-    #
-    #     bgTruth5 = {bgTruth1[key] for key in bgTruth1 if bgTruth1[key][1] >= 5}
-    #     methCnt5 = sum([bgTruth5[key][0] for key in bgTruth5])
-    #     ret5 = (methCnt5, len(bgTruth5) - methCnt5)
-    #
-    #     bgTruth10 = {bgTruth1[key] for key in bgTruth1 if bgTruth1[key][1] >= 10}
-    #     methCnt10 = sum([bgTruth10[key][0] for key in bgTruth10])
-    #     ret10 = (methCnt10, len(bgTruth10) - methCnt10)
-
-    # bgTruth5, ret5 = combineBGTruthListUsingDeepMod(bgTruthList, covCutoff=5, filterChrs=chrSet)
-    # bgTruth10, ret10 = combineBGTruthListUsingDeepMod(bgTruthList, covCutoff=10, filterChrs=chrSet)
-
-    # dataset['chr'].append(chr)
-    # dataset['Not-used.cov1'].append(ret1[2])
-    # dataset['Unmeth.cov1'].append(ret1[1])
-    # dataset['Meth.cov1'].append(ret1[0])
-    #
-    # dataset['Unmeth.cov5'].append(ret5[1])
-    # dataset['Meth.cov5'].append(ret5[0])
-    #
-    # dataset['Unmeth.cov10'].append(ret10[1])
-    # dataset['Meth.cov10'].append(ret10[0])
 
     df = pd.DataFrame.from_dict(dataset)
     logger.info(df)
@@ -233,104 +203,4 @@ if __name__ == '__main__':
     outfn = os.path.join(out_dir, f'sanity.check.using.DeepMod.paper.performance.chr_{args.chr}.threshold_{args.pred_threshold:.2f}.csv')
     df.to_csv(outfn)
     logger.info(f'save to {outfn}')
-
-    logger.info("DONE")
-    sys.exit(0)
-
-    # Combine one/two replicates, using cutoff=1 or 5
-    bgTruth = combineBGTruthList(bgTruthList, covCutoff=bgtruthCutt)
-
-    outfn = os.path.join(out_dir, f'{RunPrefix}.tss.bgtruth.cov{bgtruthCutt}.bed')
-
-    logger.info(f'Combined BS-seq data (cov>={bgtruthCutt}), all methylation level sites={len(bgTruth):,}')
-
-    output_dict_to_bed(bgTruth, outfn)
-
-    logger.info(f'\n\n####################\n\n')
-
-    callfn_dict = defaultdict()  # callname -> filename
-
-    # callname -> [call0, call1], call0 is no-filter results, call1 is filter by cutoff, and convert to [meth-freq, meth-cov] results.
-    callresult_dict = defaultdict()
-    loaded_callname_list = []
-
-    for callstr in args.calls:
-        callencode, callfn = callstr.split(':')
-
-        if len(callfn) == 0:
-            continue
-
-        callname = get_tool_name(callencode)
-        callfn_dict[callname] = callfn
-
-        # We do now allow import DeepMod.Cluster for read level evaluation
-        if callencode == 'DeepMod.C':
-            raise Exception(f'{callencode} is not allowed for site level evaluation, please use DeepMod.Cluster file here')
-
-        loaded_callname_list.append(callname)
-
-        # For site level evaluation, only need (freq, cov) results, no score needed. Especially for DeepMod, we must import as freq and cov format from DeepMod.Cluster encode
-        # TODO: cov=1 will lead to too large size of dict objects, do we really report cov=1 results?
-        # Do not filter bgtruth, because we use later for overlapping (without bg-truth)
-
-        ontCall = import_call(callfn, callencode, baseFormat=baseFormat, enable_cache=enable_cache, using_cache=using_cache, include_score=False, siteLevel=True)
-        ontCallWithCov = readLevelToSiteLevelWithCov(ontCall, minCov=minToolCovCutt, toolname=callname)
-        callresult_dict[callname] = ontCallWithCov
-        outfn = os.path.join(out_dir, f'{RunPrefix}.tss.{callname}.cov{minToolCovCutt}.bed')
-        output_dict_to_bed(ontCallWithCov, outfn)
-
-    logger.info(f'\n\n####################\n\n')
-    logger.info("TSS DONE")
-    sys.exit(0)
-
-    logger.info('Overlapping analysis start:')
-    logger.info(f"Start gen venn data for each tool (cov>={minToolCovCutt})")
-
-    # Study five set venn data, no join with bgtruth, tool-cov > tool-cutoff=1 or 3
-    if len(loaded_callname_list) >= 5:
-        cpg_set_dict = defaultdict()
-        for callname in ToolNameList:
-            cpg_set_dict[callname] = set(callresult_dict[callname][1].keys())  # .intersection(set(bgTruth.keys()))
-        gen_venn_data(cpg_set_dict, namelist=ToolNameList, outdir=out_dir, tagname=f'{RunPrefix}.{args.dsname}.five.tools.cov{minToolCovCutt}')
-
-    logger.info(f"Start gen venn data for TOP3 tools (cov>={minToolCovCutt})")
-    # Study top3 tool's venn data, no join with bgtruth, tool-cov > tool-cutoff=3
-    top3_cpg_set_dict = defaultdict()
-    for callname in Top3ToolNameList:
-        top3_cpg_set_dict[callname] = set(callresult_dict[callname][1].keys())
-    gen_venn_data(top3_cpg_set_dict, namelist=Top3ToolNameList, outdir=out_dir, tagname=f'{RunPrefix}.{args.dsname}.top3.cov{minToolCovCutt}')
-
-    logger.info(f'\n\n####################\n\n')
-
-    logger.info(f"Start getting intersection (all joined) sites by tools and bgtruth")
-    coveredCpGs = set(list(bgTruth.keys()))
-    for name in loaded_callname_list:
-        coveredCpGs = coveredCpGs.intersection(set(list(callresult_dict[name][1].keys())))
-        logger.info(f'Join {name} get {len(coveredCpGs)} CpGs')
-    logger.info(f"Reporting {len(coveredCpGs)} CpGs are covered by all tools and bgtruth")
-
-    logger.info('Output data of coverage and meth-freq on joined CpG sites for correlation analysis')
-
-    outfn_joined = os.path.join(out_dir, f"Meth_corr_plot_data_joined-{RunPrefix}-bsCov{bgtruthCutt}-minToolCov{minToolCovCutt}-baseFormat{baseFormat}.csv")
-    save_meth_corr_data(callresult_dict, bgTruth, coveredCpGs, outfn_joined)
-
-    outfn_bgtruth = os.path.join(out_dir, f"Meth_corr_plot_data_bgtruth-{RunPrefix}-bsCov{bgtruthCutt}-minToolCov{minToolCovCutt}-baseFormat{baseFormat}.csv")
-    save_meth_corr_data(callresult_dict, bgTruth, set(list(bgTruth.keys())), outfn_bgtruth)
-
-    # Report correlation matrix here
-    df = pd.read_csv(outfn_joined, sep=sep)
-    df = df.filter(regex='_freq$', axis=1)
-    cordf = df.corr()
-    logger.info(f'Correlation matrix is:\n{cordf}')
-    corr_outfn = os.path.join(out_dir, f'Meth_corr_plot_data-{RunPrefix}-correlation-matrix.xlsx')
-    cordf.to_excel(corr_outfn)
-
-    logger.info(f'\n\n####################\n\n')
-
-    # plot fig5a of correlation plot
-    command = f"set -x; PYTHONPATH=$NanoCompareDir/src python $NanoCompareDir/src/plot_figure.py fig5a -i {outfn_joined} -o {out_dir}"
-    subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).stdout.read().decode("utf-8")
-
-    logger.info(f'\n\n####################\n\n')
-
-    logger.info("### Site level correlation analysis DONE")
+    logger.info("### Sanity check DONE")
