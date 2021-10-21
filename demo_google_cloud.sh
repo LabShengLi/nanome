4bd7addb905bc8a750dc61729b0a3b9767d67861
Yang Liu <liuyang2006@gmail.com>
2021-10-20 21:13:30 -0400

    readme doc correction

--- demo_google_cloud.sh
+++ demo_google_cloud.sh
@@ -13,6 +13,27 @@
 set -ex
 date;hostname;pwd
 
+###########################################
+###########################################
+###########################################
+### Run Test pipeline on google cloud
+## working and outputs dir
+WORK_DIR_BUCKET=${1:-"gs://jax-nanopore-01-project-data/NANOME-TestData-work"}
+OUTPUT_DIR_BUCKET=${2:-"gs://jax-nanopore-01-export-bucket/NANOME-TestData-ouputs"}
+
+gsutil -m rm -rf ${WORK_DIR_BUCKET}  ${OUTPUT_DIR_BUCKET} >/dev/null 2>&1 || true
+
+## Run test demo on google cloud
+nextflow run main.nf\
+    -profile docker,google \
+	-w ${WORK_DIR_BUCKET} \
+	--outputDir ${OUTPUT_DIR_BUCKET} \
+	--dsname TestData \
+	--input https://raw.githubusercontent.com/TheJacksonLaboratory/nanome/master/inputs/test.demo.filelist.txt
+
+echo "### nanome pipeline for demo data on google DONE"
+
+exit 0
 
 ###########################################
 ###########################################
@@ -37,24 +58,3 @@ nextflow run main.nf\
 
 echo "### nanome pipeline for NA12878 some chr and part file on google DONE"
 exit 0
-
-
-###########################################
-###########################################
-###########################################
-### Run Test pipeline on google cloud
-## working and outputs dir
-WORK_DIR_BUCKET=${1:-"gs://jax-nanopore-01-project-data/NANOME-TestData-work"}
-OUTPUT_DIR_BUCKET=${2:-"gs://jax-nanopore-01-export-bucket/NANOME-TestData-ouputs"}
-
-gsutil -m rm -rf ${WORK_DIR_BUCKET}  ${OUTPUT_DIR_BUCKET} >/dev/null 2>&1 || true
-
-## Run test demo on google cloud
-nextflow run main.nf\
-    -profile docker,google \
-	-w ${WORK_DIR_BUCKET} \
-	--outputDir ${OUTPUT_DIR_BUCKET} \
-	--dsname TestData \
-	--input https://raw.githubusercontent.com/TheJacksonLaboratory/nanome/master/inputs/test.demo.filelist.txt
-
-echo "### nanome pipeline for demo data on google DONE"
